32c5c525a029149240f584f5b983e675df083f87
Igor Gorodetsky <igorodetsky@a-group.az>
2022-04-01 16:35:26 +0400

    Add wrapper

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php
+    include "./include/header.php";
+?>
 <body>
-    stalker    
+    <div class="wrapper">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis delectus blanditiis. Odit illo cupiditate, nobis numquam voluptatibus praesentium laborum corporis odio, reprehenderit autem ab quis saepe porro tenetur. Hic!
+    </div>    
 </body>
 </html>
