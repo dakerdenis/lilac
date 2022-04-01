559dd77b4f347e7b4d7ad28faab49779bae3f5ce
Igor Gorodetsky <igorodetsky@a-group.az>
2022-04-01 17:24:36 +0400

    change text

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
 <?php include "./include/header.php"; ?>
 <?php include "./include/navigation.php"; ?>
+
+
+<!---- ......WRAPPER----->
     <div class="wrapper">
-        test 1234
-    </div>    
-</body>
-</html>
+        
+    <!---FIRST BLOCK PROMO NAME AND PHOTO--->
+        <div class="block__erste_name">
+
+        </div>
+
+
+
+    <!-- THREE BLOCKS PROMO --->
+        <div class="block__zweite-promo">
+
+        </div>
+
+
+    <!--- ABOUT US AND WHAT WE DO ---->
+        <div class="block__dritte-wedo">
+
+        </div>
+
+    <!-- CATALOGUE ABOUT US --->
+        <div class="block__vierte-catalogue">
+
+        </div>
+
+    <!--- CONNECT WITH US BLOCK--->
+        <div class="block__funfte-connect">
+
+        </div>
+
+
+
+    <!---FOOTER END OF PAGE--------------- -->
+    <?php
+        include "./include/footer.php"
+    ?>
