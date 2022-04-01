7a2cc91ef3b54e4860bf9e3dcab99b5a475bc946
Igor Gorodetsky <igorodetsky@a-group.az>
2022-04-01 17:11:54 +0400

    change text

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
     include "./include/header.php";
 ?>
 <body>
+    <?php 
+        include "./include/navigation.php";
+    ?>
     <div class="wrapper">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis delectus blanditiis. Odit illo cupiditate, nobis numquam voluptatibus praesentium laborum corporis odio, reprehenderit autem ab quis saepe porro tenetur. Hic!
+        .
     </div>    
 </body>
 </html>
