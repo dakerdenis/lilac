56687d369a6ce387153b815d29d6ff5cc2aaff05
Denis Daker <dakerdenis@gmail.com>
2022-04-02 12:08:31 +0400

    add

--- index.php
+++ index.php
@@ -7,23 +7,39 @@
         
     <!---FIRST BLOCK PROMO NAME AND PHOTO--->
         <div class="block__1-name">
-
+            <!-- DIV WITH IMAGE--->
+                <div class="block__name__wrapper">
+                    <!-- DIV WITH NAME AND DESC-->
+                    <div class="block__name__text">
+                        <span class="block__name__text-name">LILAC handmade</span>
+                        <span class="block__name__text-des">♥ Подарки ручной работы ♥</span>
+                        <span class="block__name__text-by">By Narmina Huseynli</span>
+                    </div>
+                </div>
         </div>
-
-
+    <!----------------------------------------->
+        <div class="block__photo__1">
+                <div class="block__photo__1-text">
+                Подарок созданный в ручную - уникальный.
+                </div>
+        </div>
 
     <!-- THREE BLOCKS PROMO --->
         <div class="block__2-promo">
-
+            <div class="block__2-promo-wrapper">
+                <div class="block__2-promo-1"></div>
+                <div class="block__2-promo-2"></div>
+                <div class="block__2-promo-3"></div>
+            </div>
         </div>
-
+    <!----------------------------------------->
 
     <!--- ABOUT US AND WHAT WE DO ---->
         <div class="block__3-wedo">
 
         </div>
 
-    <!-- CATALOGUE ABOUT US --->
+    <!-- CATALOGUE OF ELEMENTS  --->
         <div class="block__4-catalogue">
 
         </div>
