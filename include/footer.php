76e91cebbecc1194de43721f67819d1d50dbb777
Denis Daker <dakerdenis@gmail.com>
2022-04-08 12:21:04 +0400

    add

--- include/footer.php
+++ include/footer.php
@@ -4,5 +4,23 @@
     </div><!-- WRAPPER ENDS-->
 
     <script src="https://kit.fontawesome.com/5480a04e88.js" crossorigin="anonymous"></script>
+      <!-- Swiper JS -->
+      <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    </script>
 </body>
 </html>
