76fe219aefafe96fa30c4554c5240e494f8fda4d
Denis Daker <dakerdenis@gmail.com>
2022-04-08 12:20:50 +0400

    add

---
+++ include/catalog.php
@@ -0,0 +1,119 @@
+<?php
+    include "./vendor/db.php";
+    
+    ?>
+
+
+<div class="catalog_wrapper">
+    <!--Catalog name -->
+
+    <div class="catalog__name">
+        <p class="catalog__p">
+            Каталог
+        </p>
+    </div>
+    <!-- Swiper -->
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="catalog__wrapper">
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="catalog__wrapper">
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="catalog__wrapper">
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                    <!----1111----->
+                    <div class="catalog_wrapper_element">
+                        <a href="#">
+                            <div class="catalog_wrapper_element-img"></div>
+                            <div class="catalog_wrapper_element-text"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
