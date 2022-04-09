65c2b642bcd32d6a813ab84720e363cab08f2fa0
Denis Daker <dakerdenis@gmail.com>
2022-04-09 23:30:58 +0400

    add

--- include/catalog.php
+++ include/catalog.php
@@ -11,6 +11,9 @@
         <p class="catalog__p">
             Каталог
         </p>
+        <p class="catalog__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ea itaque eveniet molestiae quibusdam nihil laudantium quasi consequuntur modi quos, tenetur provident hic numquam fuga totam autem, accusamus quidem libero.
+        </p>
     </div>
     <!-- Swiper -->
     <div class="swiper mySwiper">
@@ -47,8 +50,9 @@
                     </div>
                 </div>
             </div>
+            
             <div class="swiper-slide">
-            <div class="catalog__wrapper">
+                <div class="catalog__wrapper">
                     <!----1111----->
                     <div class="catalog_wrapper_element">
                         <a href="#">
@@ -80,7 +84,7 @@
                 </div>
             </div>
             <div class="swiper-slide">
-            <div class="catalog__wrapper">
+                <div class="catalog__wrapper">
                     <!----1111----->
                     <div class="catalog_wrapper_element">
                         <a href="#">
@@ -114,6 +118,9 @@
         </div>
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
+
+    </div>
+    <div class="catalog__button">
+        <a href="./include/full_catalog.php" class="catalog__link">К полному каталогу</a>
     </div>
 </div>
