c84df24ac7864cefe2d5812a85a99b32a2adf780
Denis Daker <dakerdenis@gmail.com>
2022-04-12 12:49:43 +0400

    add

--- include/footer.php
+++ include/footer.php
@@ -1,16 +1,30 @@
 <div class="block__footer__end">
 
     <div class="footer__content_wrapper">
-        <div class="footer__content__text">
 
-        </div>
 
         <div class="footer__content__links">
-            <a href="" class="footer__content_link"></a>
-            <a href="" class="footer__content_link"></a>
-            <a href="" class="footer__content_link"></a>
-            <a href="" class="footer__content_link"></a>
+            <a href="" class="footer__content_link">
+                О нас
+            </a>
+            <a href="" class="footer__content_link">
+                Политика конфиденциальности
+            </a>
+            <a href="" class="footer__content_link">
+                Соглашения
+            </a>
+            <a href="" class="footer__content_link">
+                Отзывы
+            </a>
+            <a href="" class="footer__content_link">
+                Стать партнером
+            </a>
         </div>
+        <div class="footer__content__text">
+                <p class="footer__content__text-p">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ratione necessitatibus iusto reprehenderit nemo quaerat nam molestiae nostrum alias autem cupiditate assumenda ea voluptatibus hic, facere perferendis. Rem, unde delectus.
+                </p>
+            </div>
     </div>
 
     <div class="block__footer__end_wrapper">
